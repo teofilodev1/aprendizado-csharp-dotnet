c4ddc51482c129da58cbae5325102af6ab4b0ec5
João Pedro Teofilo De Campos <joaopedroteofilo90@gmail.com>
2026-05-09 12:24:01 -0300

    feat: resolve issue #1 - tipos básicos e interpolação

---
+++ exercicio_1/Exercicio 1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace exercicio_1;
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Digite seu nome:");
+
+        string nome = Console.ReadLine();
+        int letras = nome.Length;
+
+        Console.WriteLine($"Olá { nome }, seu nome tem { letras } caracterese no nome");
+    }
+
+}
