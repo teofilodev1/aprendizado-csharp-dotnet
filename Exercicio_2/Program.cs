428753f713a4d37736b815a2e3e2ade4ec455f6e
jpfullstack <joaopedroteofilo90@gmail.com>
2026-05-10 17:43:43 -0300

    feat: resolve issue 2 - condicionais

---
+++ Exercicio_2/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Exercicio_2;
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Digite um numero:");
+        double a = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Digite outro numero:");
+        double b = Convert.ToDouble(Console.ReadLine());
+        double result2 = a + b;
+
+        if (result2 % 1 == 0)
+        {
+            Console.WriteLine("O resultado é inteiro: " + result2);
+        }
+        else
+        {
+            Console.WriteLine("O resultado é racional: " + result2);
+        }
+    }
+}
