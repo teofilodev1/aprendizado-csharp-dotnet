3145fe97504186f18a95824682f26c409c22fc89
jpfullstack <joaopedroteofilo90@gmail.com>
2026-05-10 18:11:25 -0300

    feat: resolve issue 3 - Loops e contadores

---
+++ exercicio_3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Exercicio_3;
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Digite um número:");
+        int a = int.Parse(Console.ReadLine());
+        int contador = 1;
+
+        for (int i = 1; i < 11; i++)
+        {
+            Console.WriteLine($"{a} x {i} = {a * i}");
+        }
+
+        Console.WriteLine("====================");
+        Console.WriteLine($"Tabuada do Numero: {a}");
+        while (contador <= 10)
+        {
+            Console.WriteLine($"{a} x {contador} = {a * contador}");
+            contador++;
+        }
+    }
+}
